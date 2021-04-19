af3e776b49d0aac32ae30c8057792c8a3141b12a
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-04-19 14:23:56 +0630

    Dynamic Flash Component Example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
-    <x-section>
-        Hello
-    </x-section>
-    <x-section>
-        Hi
-    </x-section>
-    <x-section>
-        First Slotting Component
-    </x-section>
+
+    <x-flash type='error' data-flash="true">
+        Login Success
+    </x-flash>
+
+    <x-flash type='warning' class="mt-20">
+        Login Success
+    </x-flash>
+    
 </x-layout>
